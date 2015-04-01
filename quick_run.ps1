fb469603e91f3f8941c19aa4a05b5c26e65c87e2
Suremaker <wojciech.kotlarski@gmail.com>
2015-04-01 23:50:56 +0100

    updated quick_run.ps1

--- quick_run.ps1
+++ quick_run.ps1
@@ -1,4 +1,4 @@
 rmdir temp -force -recurse -ErrorAction SilentlyContinue
 & .\packageAll.ps1
-& .\psmake\psmake.ps1 -Scaffold empty -md temp -Source $PSScriptRoot
-& .\temp\make.ps1 -Target build -md temp
+& .\psmake\psmake.ps1 -Scaffold empty -md temp -NuGetSource $PSScriptRoot
+& .\temp\make.ps1 -Target build
