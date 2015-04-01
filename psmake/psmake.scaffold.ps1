b702afcda83a3627a6a18097d578cd10d8120f96
wojtek <wojciech.kotlarski@wonga.com>
2015-04-01 14:43:50 +0100

    improved scaffolding

--- psmake/psmake.scaffold.ps1
+++ psmake/psmake.scaffold.ps1
@@ -4,11 +4,33 @@ function Scaffold-Empty($psmakeVersion)
     mkdir $Context.MakeDirectory -ErrorAction SilentlyContinue | Out-Null
     
     Write-Host "Creating make.ps1..."
-    $nuArgs=$Context.NuGetArgs
+    $nuArgs=if($Context.NuGetConfig) { "-ConfigFile $($Context.NuGetConfig)"} else { '' } 
     $cmd = "$($Context.NuGetExe) install psmake -Version $psmakeVersion -OutputDirectory $($Context.MakeDirectory)\psmake -Verbosity detailed $nuArgs"  
     $file = "$($Context.MakeDirectory)\make.ps1"
-    Write-Output "$cmd" | Out-File $file
-    Write-Output "$($Context.MakeDirectory)\psmake\psmake.$psmakeVersion\psmake.ps1 -md $($Context.MakeDirectory) `@args" | Out-File $file -append
+	$nugetSrc = if ($Context.NuGetSource) { $Context.NuGetSource } else { '$null' }
+	
+	$content = @"
+`$private:PsMakeVer = '$psmakeVersion'
+`$private:PsMakeNugetSource = '$nugetSrc'
+
+function private:Get-NuGetArgs (`$params, `$defaultSource)
+{
+	function private:Find-Arg(`$array, `$name, `$default) { 
+		`$idx = [array]::IndexOf(`$array, `$name)
+		if( (`$idx -gt -1) -and (`$idx+1 -lt `$array.Length)) { return `$array[`$idx+1] }
+		return `$default
+	}
+	`$nuGetSource = Find-Arg `$params '-NuGetSource' `$defaultSource
+	if (`$nuGetSource) { return '-Source', `$nuGetSource}
+	return @()
+}
+
+`$private:srcArgs = Get-NuGetArgs `$args `$PsMakeNugetSource
+$($Context.NuGetExe) install psmake -Version `$PsMakeVer -OutputDirectory $($Context.MakeDirectory) $nuArgs @srcArgs
+& `"$($Context.MakeDirectory)\psmake.`$PsMakeVer\psmake.ps1`" -md $($Context.MakeDirectory) @args
+"@
+	
+    Write-Output $content | Out-File $file
 
     Write-Host "Creating Makefile.ps1..."
     $file = "$($Context.MakeDirectory)\Makefile.ps1"
