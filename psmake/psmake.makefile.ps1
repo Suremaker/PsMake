ab31fa4c467e0b83aabfe7b065c02d9767ef22ed
wojtek <wojciech.kotlarski@wonga.com>
2015-04-01 10:18:08 +0100

    simplified psmake.makefile.ps1

--- psmake/psmake.makefile.ps1
+++ psmake/psmake.makefile.ps1
@@ -1,13 +1,4 @@
 function Define-Step($name, $body, $target)
 {
-	$object = New-Object PSObject
-	Add-Member -InputObject $object -MemberType NoteProperty -Name Name -Value ""
-	Add-Member -InputObject $object -MemberType NoteProperty -Name Target -Value ""
-	Add-Member -InputObject $object -MemberType NoteProperty -Name Body -Value ""
-	 
-	$object.Name = $name
-	$object.Target=$target -split ","
-	$object.Body=$body
-
-	Write-Output $object
+	Write-Output Create-Object @{ Name=$name; Target=$target -split ","; Body=$body} 
 }
