fcbd161b797b679c50342dc06a987d042ade9430
wojtek <wojciech.kotlarski@wonga.com>
2015-09-24 17:58:44 +0100

    added psmake.mod.update-assembly-info-version

---
+++ psmake.mod.update-assembly-info-version.Tests/psmake.mod.update-assembly-info-version.Tests.ps1
@@ -0,0 +1,38 @@
+$here = (Split-Path -Parent $MyInvocation.MyCommand.Path).Replace(".Tests", "")
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\..\psmake\ext\psmake.core.ps1"
+. "$here\$sut"
+
+Describe "Update-AssemblyInfoVersion" {
+    
+    It "It should update AsseblyInfo.cs versions" {
+		rmdir "$PSScriptRoot\proj" -Force -Recurse -ErrorAction "SilentlyContinue"
+		rmdir "$PSScriptRoot\proj2" -Force -Recurse -ErrorAction "SilentlyContinue"
+		mkdir "$PSScriptRoot\proj"
+		mkdir "$PSScriptRoot\proj2"
+		mkdir "$PSScriptRoot\proj\inner"
+
+		set-content "$PSScriptRoot\proj2\AssemblyInfo.cs" "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
+		set-content "$PSScriptRoot\proj\AssemblyInfo.cs" "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0"")]`n[assembly: AssemblyFileVersion(""1.0.0"")]"
+		set-content "$PSScriptRoot\proj\inner\AssemblyInfo.cs" "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
+		set-content "$PSScriptRoot\proj\OtherFile.cs" "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
+
+		Update-AssemblyInfoVersion '4.3.2.1' "$PSScriptRoot\proj"
+		
+		(get-content "$PSScriptRoot\proj2\AssemblyInfo.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
+        (get-content "$PSScriptRoot\proj\AssemblyInfo.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""4.3.2.1"")]`n[assembly: AssemblyFileVersion(""4.3.2.1"")]"
+		(get-content "$PSScriptRoot\proj\inner\AssemblyInfo.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""4.3.2.1"")]`n[assembly: AssemblyFileVersion(""4.3.2.1"")]"
+		(get-content "$PSScriptRoot\proj\OtherFile.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
+    }
+}
+
+Describe "Update-VersionInFile" {
+    
+    It "It should update file versions" {
+		set-content "$PSScriptRoot\file.txt" "Version(abc), Version(1), Version(1.2), Version(1.2.3), Version(1.2.3.4), Version(1.2.3.4.555), Other(3.2), Else(1.2)"
+		
+		Update-VersionInFile "$PSScriptRoot\file.txt" '3.2.1' 'Version(%)','Other(%)'
+		
+		get-content "$PSScriptRoot\file.txt" | Should Be "Version(abc), Version(3.2.1), Version(3.2.1), Version(3.2.1), Version(3.2.1), Version(1.2.3.4.555), Other(3.2.1), Else(1.2)"
+    }
+}
---
+++ psmake.mod.update-assembly-info-version/psmake.mod.update-assembly-info-version.ps1
@@ -0,0 +1,85 @@
+<#
+.SYNOPSIS 
+Replaces version string in a specified file.
+
+.DESCRIPTION
+Replaces version string in a specified file.
+It allows to replace a version string occurrences in given file to a version specified by Version parameter
+
+.EXAMPLE
+PS> Update-InFile 'project\AssemblyVersion.cs' '1.0.0.1' 'Version("%")'"
+#>
+function Update-VersionInFile
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, Position=0)]
+        # File path. 
+        [ValidateNotNullOrEmpty()]
+        [string]$File,
+
+        [Parameter(Mandatory=$true, Position=1)]
+        # New version
+        [ValidateNotNullOrEmpty()]
+        [ValidatePattern("^[0-9]+(\.[0-9]+){0,3}$")]
+        [string]$Version,
+
+        [Parameter(Mandatory=$true, Position=2)]
+        # Matchings representing patterns with version string, where % represents version itself.
+        # Example: 'Version("%")'
+        [ValidateNotNullOrEmpty()]
+        [string[]]$matchings
+    )
+
+	Write-ShortStatus "Updating $File with $Version..."
+	$content = Get-Content $File -Encoding UTF8
+	foreach($match in $matchings)
+	{
+		$from = [Regex]::Escape($match) -replace '%','[0-9]+(\.[0-9]+){0,3}'
+		$to = $match -replace '%',$Version
+
+		$content = $content -replace $from,$to
+	}
+	Set-Content $File -Value $content -Encoding UTF8
+}
+
+<#
+.SYNOPSIS 
+Replaces version string all AssemblyInfo.cs files located in specified solution directory.
+
+.DESCRIPTION
+Replaces version string all AssemblyInfo.cs files located in specified solution directory.
+
+.EXAMPLE
+PS> Update-AssemblyInfoVersion '1.0.0.22' 'slnDir'"
+#>
+function Update-AssemblyInfoVersion
+{
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$true)]
+		# Version to set to
+		[ValidateNotNullOrEmpty()]
+		[ValidatePattern("^[0-9]+(\.[0-9]+){0,3}$")]
+		[string]$Version,
+		
+		[Parameter()]
+		# Solution directory
+		[string]$SolutionDirectory = "."
+	) 
+	function Update-SourceVersion ($version)
+	{
+		foreach ($o in $input) 
+		{
+			Update-VersionInFile $o.FullName $version 'Version("%")'
+		}
+	}
+
+	function Update-AllAssemblyInfoFiles ( $version )
+	{
+		get-childitem -Path $SolutionDirectory -recurse |? {$_.Name -eq "AssemblyInfo.cs"} | Update-SourceVersion $Version
+	}
+
+	Write-Status "Updating all AssemblyInfo.cs version to $version in $SolutionDirectory directory"
+	Update-AllAssemblyInfoFiles $Version;
+}
