c22f6e4050ae55d41794d1d3283991c7e80300f5
William Lord <williamlord@users.noreply.github.com>
2016-02-16 14:45:35 +0000

    Extend Update-VersionInAssemblyInfo to allow assembly info file names to be specified

--- psmake.mod.update-version-info.Tests/psmake.mod.update-version-info.Tests.ps1
+++ psmake.mod.update-version-info.Tests/psmake.mod.update-version-info.Tests.ps1
@@ -21,7 +21,8 @@ function Write-Host
 
 Describe "Update-VersionInAssemblyInfo" {
     
-    It "It should update AsseblyInfo.cs versions" {
+    function SetupAssemblyInfoFiles
+    {
         rmdir "$PSScriptRoot\proj" -Force -Recurse -ErrorAction "SilentlyContinue"
         rmdir "$PSScriptRoot\proj2" -Force -Recurse -ErrorAction "SilentlyContinue"
         mkdir "$PSScriptRoot\proj"
@@ -31,14 +32,34 @@ Describe "Update-VersionInAssemblyInfo" {
         set-content "$PSScriptRoot\proj2\AssemblyInfo.cs" "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
         set-content "$PSScriptRoot\proj\AssemblyInfo.cs" "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0"")]`n[assembly: AssemblyFileVersion(""1.0.0"")]"
         set-content "$PSScriptRoot\proj\inner\AssemblyInfo.cs" "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
+        set-content "$PSScriptRoot\proj\inner\OtherFile.cs" "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
         set-content "$PSScriptRoot\proj\OtherFile.cs" "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
+    }
+	
+    It "It should update AsseblyInfo.cs versions" {
+        
+        SetupAssemblyInfoFiles
 
         Update-VersionInAssemblyInfo '4.3.2.1' "$PSScriptRoot\proj"
         
         (get-content "$PSScriptRoot\proj2\AssemblyInfo.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
         (get-content "$PSScriptRoot\proj\AssemblyInfo.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""4.3.2.1"")]`n[assembly: AssemblyFileVersion(""4.3.2.1"")]"
         (get-content "$PSScriptRoot\proj\inner\AssemblyInfo.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""4.3.2.1"")]`n[assembly: AssemblyFileVersion(""4.3.2.1"")]"
+        (get-content "$PSScriptRoot\proj\inner\OtherFile.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
         (get-content "$PSScriptRoot\proj\OtherFile.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
+    }
+	
+	It "It should update versions in specified assembly info files" {
+
+        SetupAssemblyInfoFiles
+
+        Update-VersionInAssemblyInfo '4.3.2.1' "$PSScriptRoot\proj" ("OtherFile.cs")
+        
+        (get-content "$PSScriptRoot\proj2\AssemblyInfo.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
+        (get-content "$PSScriptRoot\proj\AssemblyInfo.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0"")]`n[assembly: AssemblyFileVersion(""1.0.0"")]"
+        (get-content "$PSScriptRoot\proj\inner\AssemblyInfo.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""1.0.0.0"")]`n[assembly: AssemblyFileVersion(""1.0.0.0"")]"
+        (get-content "$PSScriptRoot\proj\inner\OtherFile.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""4.3.2.1"")]`n[assembly: AssemblyFileVersion(""4.3.2.1"")]"
+        (get-content "$PSScriptRoot\proj\OtherFile.cs") -join "`n" | Should Be "[assembly: SomethingOther(""1.0"")]`n[assembly: AssemblyVersion(""4.3.2.1"")]`n[assembly: AssemblyFileVersion(""4.3.2.1"")]"
     }
 }
 
@@ -46,7 +67,7 @@ Describe "Update-VersionInFile" {
     
     It "It should update file versions" {
         set-content "$PSScriptRoot\file.txt" "Version(abc), Version(1), Version(1.2), Version(1.2.3), Version(1.2.3.4), Version(1.2.3.4.555), Other(3.2), Else(1.2)"
-        
+
         Update-VersionInFile "$PSScriptRoot\file.txt" '3.2.1' 'Version(%)','Other(%)'
         
         get-content "$PSScriptRoot\file.txt" | Should Be "Version(abc), Version(3.2.1), Version(3.2.1), Version(3.2.1), Version(3.2.1), Version(1.2.3.4.555), Other(3.2.1), Else(1.2)"
